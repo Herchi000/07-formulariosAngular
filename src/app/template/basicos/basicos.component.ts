a11a556dc4bc24cce48a0c4da23759f700b5a19d
Emanuel <hugofer000@gmail.com>
2022-07-13 14:05:30 -0300

    07-formularios 02

--- src/app/template/basicos/basicos.component.ts
+++ src/app/template/basicos/basicos.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { NgForm } from '@angular/forms';
 
 @Component({
@@ -9,14 +9,26 @@ import { NgForm } from '@angular/forms';
 })
 export class BasicosComponent implements OnInit {
 
+  @ViewChild('miFormulario') miFormulario!: NgForm;
+
   constructor() { }
 
   ngOnInit(): void {
   }
 
-  guardar(miFormulario: NgForm){
-    console.log(miFormulario);
+  //guardar(miFormulario: NgForm){
+  guardar(){
+    console.log(Number(this.miFormulario?.controls['precio']?.value));
     
+  }
+
+  nombreValido() :Boolean{
+    return this.miFormulario?.form.controls['producto']?.invalid && this.miFormulario?.controls['producto']?.touched;
+  }
+
+  precioValido() :Boolean{
+    return this.miFormulario?.controls['precio']?.touched &&
+           this.miFormulario?.controls['precio']?.value <= 0 ;
   }
 
 
